0156d2df004b93cc95b010f9c7d4ced1ae15e0b4
Aghatinha <aghata.paulo@etec.sp.gov.br>
2023-03-30 08:53:12 -0300

    Criação

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'telefoneApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
